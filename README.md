cbe6a51f873c1e7d616c2e87a99e413caac7348b
Grant Rettke <gcr@wisdomandwonder.com>
2016-12-18 15:26:17 -0600

    Recursively notify of uncommitted or unpushed changed

---
+++ README.md
@@ -0,0 +1,1 @@
+Report Git working copies with uncommitted or unpushed changes in all sub-directories
--- recursive-git-status.sh
+++ recursive-git-status.sh
@@ -1,3 +1,18 @@
 #!/usr/local/bin/bash
 # -*- mode: sh; -*-
 
+declare rgs_owd="$PWD"
+declare -a rgs_gitdirs
+rgs_gitdirs=($(find $rgs_owd -type d -name '.git' | sort --ignore-case))
+for gitdir in "${rgs_gitdirs[@]}"; do
+    cd "$gitdir"
+    cd ..
+    if [[ $(git status --porcelain) || $(git status | grep 'ahead') ]]; then
+        echo ">>>>>>>>> $PWD"
+    else
+        echo "          $PWD"
+    fi
+done
+cd "$rgs_owd"
+unset rgs_owd
+unset rgs_gitdirs
